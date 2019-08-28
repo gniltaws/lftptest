5dfb72c9253ceb20c07384d122f0b822d71dce6f
Todd Swatling <swatling@buffalo.edu>
2019-08-28 10:22:29 -0400

    force ssl

--- mirror.sh
+++ mirror.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 lftp ftps://${box_user}:${box_password}@ftp.box.com:21 << here_doc
+  set ftp:ssl-force
   mirror --loop -v --reverse --parallel=$concurrent_uploads --no-perms "${local_base_dir}" "${box_base_dir}/"
   exit
 here_doc
