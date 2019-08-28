29e5afdf9519b204ca30fac6deff72fa6457501f
Todd Swatling <swatling@buffalo.edu>
2019-08-28 10:25:13 -0400

    more ssl options

--- mirror.sh
+++ mirror.sh
@@ -2,6 +2,8 @@
 
 lftp ftps://${box_user}:${box_password}@ftp.box.com:21 << here_doc
   set ftp:ssl-force
+  set ssl:check-hostname false
+  set ftp:ssl-protect-data true
   mirror --loop -v --reverse --parallel=$concurrent_uploads --no-perms "${local_base_dir}" "${box_base_dir}/"
   exit
 here_doc
