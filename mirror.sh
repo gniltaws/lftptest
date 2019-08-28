8600ec9651e5a5abae8955491962c264f938f79d
Todd Swatling <swatling@buffalo.edu>
2019-08-28 10:09:39 -0400

    changed port

--- mirror.sh
+++ mirror.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-lftp ftps://${box_user}:${box_password}@ftp.box.com:990 << here_doc
+lftp ftps://${box_user}:${box_password}@ftp.box.com:21 << here_doc
   mirror --loop -v --reverse --parallel=$concurrent_uploads --no-perms "${local_base_dir}" "${box_base_dir}/"
   exit
 here_doc
